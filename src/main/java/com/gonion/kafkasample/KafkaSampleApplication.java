f874980d1e0d69597247eab0e56af28a4030016c
Quan Vo <tranminhquanvo@gmail.com>
2024-01-28 13:31:22 +0700

    Initialize the project

---
+++ src/main/java/com/gonion/kafkasample/KafkaSampleApplication.java
@@ -0,0 +1,11 @@
+package com.gonion.kafkasample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSampleApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(KafkaSampleApplication.class, args);
+  }
+}
---
+++ src/test/java/com/gonion/kafkasample/KafkaSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonion.kafkasample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
